9d518ed29906adfdbc02fa8d51c4f485fe1968e6
arunkumar <arunkumar.gudelli@gmail.com>
2022-08-30 14:42:33 +0530

    updated urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  imagesCount: string[] = [];
+
+  imageUrls:imageInfo[] = [];
+
+  randomContent = "";
 
   constructor() {
+    var baseUrl = "https://picsum.photos/1024/768?random=";
     for (let i = 0; i < 10; i++) {
-      this.imagesCount.push("class-" + i);
+      this.imageUrls.push({
+        id:"class"+i,
+        url:baseUrl+i
+      });
     }
+
+    this.randomContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
   }
+}
+
+export interface imageInfo{
+  id:string;
+  url:string;
 }
